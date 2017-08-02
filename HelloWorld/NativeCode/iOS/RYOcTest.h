10bda43bf5849e1bf9c8a1d08a8909bb83aea26c
RyukieSama <ryukie.sama@gmail.com>
2017-08-02 13:53:43 +0800

    file add

---
+++ HelloWorld/NativeCode/iOS/RYOcTest.h
@@ -0,0 +1,15 @@
+//
+//  RYOcTest.h
+//  hello_world
+//
+//  Created by 王荣庆 on 2017/8/2.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RYOcTest : NSObject
+
++ (void)ryLog:(NSString *)content;
+
+@end
---
+++ HelloWorld/NativeCode/iOS/RYOcTest.m
@@ -0,0 +1,17 @@
+//
+//  RYOcTest.m
+//  hello_world
+//
+//  Created by 王荣庆 on 2017/8/2.
+//
+//
+
+#import "RYOcTest.h"
+
+@implementation RYOcTest
+
++ (void)ryLog:(NSString *)content {
+    NSLog(@"%@",content);
+}
+
+@end
