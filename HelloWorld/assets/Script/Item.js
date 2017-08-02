864faa0a0daaa6b52b128965a31c7639788c0b11
RyukieSama <ryukie.sama@gmail.com>
2017-08-02 13:42:18 +0800

    OC CC bridge

--- HelloWorld/assets/Script/Item.js
+++ HelloWorld/assets/Script/Item.js
@@ -33,6 +33,11 @@ cc.Class({
 
         text : "avatar",
 
+        coverButton : {
+            default : null,
+            type : cc.Button
+        },
+
     },
 
     // use this for initialization
@@ -40,11 +45,21 @@ cc.Class({
         let index = this.getRandNumber(1,12);
         this.label.string = "Name:avatar" + index.toString();
         this.avatar.spriteFrame = this.avatars[index-1];
+        this.coverButton.node.on('click', this.callback, this);
     },
 
     getRandNumber : function (min,max) {
         return Math.floor(min+Math.random()*(max-min));
     },
+
+    callback: function (event) {
+        //这里的 event 是一个 EventCustom 对象，你可以通过 event.detail 获取 Button 组件
+
+        var result = jsb.reflection.callStaticMethod("RYOcTest", "ryLog:", "HAHAHAHAHAHAH");
+
+        //do whatever you want with button
+        //另外，注意这种方式注册的事件，也无法传递 customEventData
+    }
 
     //
     // init : function () {
